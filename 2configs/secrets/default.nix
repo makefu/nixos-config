ac112d6c21fa692c64afc9b27c4af4ea552f2d4d
makefu <github@syntax-fehler.de>
2023-06-11 00:47:05 +0200

    secrets: add default

---
+++ 2configs/secrets/default.nix
@@ -0,0 +1,4 @@
+{
+  sops.defaultSopsFile = ../../secrets/common.yaml;
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+}
--- 2configs/secrets/user-passwords.nix
+++ 2configs/secrets/user-passwords.nix
@@ -1,12 +1,12 @@
 { config, ... }:
 {
-  sops.defaultSopsFile = ../../secrets/common.yaml;
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  imports = [ ./default.nix ];
 
   sops.secrets = {
     "passwd/makefu".neededForUsers = true;
     "passwd/root".neededForUsers = true;
   };
+
   users.users = {
     makefu.passwordFile = config.sops.secrets."passwd/makefu".path;
     root.passwordFile = config.sops.secrets."passwd/root".path;
