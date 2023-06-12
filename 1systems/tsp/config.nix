db6423f146b140c099d12708bcc1b94e72bbe278
makefu <github@syntax-fehler.de>
2023-06-13 00:30:42 +0200

    tsp.r: rebuild with flakes

--- 1systems/tsp/config.nix
+++ 1systems/tsp/config.nix
@@ -1,45 +1,35 @@
-#
-#
-#
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, self, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware.nix
-      <stockholm/makefu>
-      <stockholm/makefu/2configs/nur.nix>
-      <stockholm/makefu/2configs/home-manager>
-      <stockholm/makefu/2configs/main-laptop.nix>
-      <stockholm/makefu/2configs/editor/neovim>
-      <stockholm/makefu/2configs/tools/core.nix>
-      # <stockholm/makefu/2configs/tools/all.nix>
-      <stockholm/makefu/2configs/fs/single-partition-ext4.nix>
+      ../../2configs/default.nix
+            # (self + "/2configs/nur.nix")
+
+      # (self + "/2configs/nur.nix")
+      (self + "/2configs/home-manager")
+      (self + "/2configs/main-laptop.nix")
+      (self + "/2configs/editor/neovim")
+      (self + "/2configs/tools/core.nix")
+      # (self + "/2configs/tools/all.nix")
+      (self + "/2configs/fs/disko/single-disk-bcachefs.nix")
       # hardware specifics are in here
-      <stockholm/makefu/2configs/hw/bluetooth.nix>
-      <stockholm/makefu/2configs/hw/network-manager.nix>
+      (self + "/2configs/hw/bluetooth.nix")
+      (self + "/2configs/hw/network-manager.nix")
 
 
-      # <stockholm/makefu/2configs/rad1o.nix>
+      # (self + "/2configs/rad1o.nix")
 
-      <stockholm/makefu/2configs/zsh-user.nix>
-      <stockholm/makefu/2configs/home-manager>
-      <stockholm/makefu/2configs/home-manager/desktop.nix>
-      <stockholm/makefu/2configs/home-manager/cli.nix>
-      <stockholm/makefu/2configs/tinc/retiolum.nix>
+      (self + "/2configs/zsh-user.nix")
+      (self + "/2configs/home-manager")
+      (self + "/2configs/home-manager/desktop.nix")
+      (self + "/2configs/home-manager/cli.nix")
 
-      <stockholm/makefu/2configs/sshd-totp.nix>
-      {
-        programs.adb.enable = true;
-      }
+      # still broken
+      #(self + "/2configs/tinc/retiolum.nix")
+      # (self + "/2configs/sshd-totp.nix")
     ];
-  krebs.build.host = config.krebs.hosts.tsp;
-  boot.loader.grub.device = "/dev/sda";
 
-  networking.firewall.allowedTCPPorts = [
-    25
-  ];
-
-  hardware.enableRedistributableFirmware = true;
   nixpkgs.config.allowUnfree = true;
 }
--- 1systems/tsp/hardware.nix
+++ 1systems/tsp/hardware.nix
@@ -2,8 +2,9 @@
 {
   imports = [
     # laptop is an acer aspire, but close enough i'd say
-    <stockholm/makefu/2configs/hw/tp-x2x0.nix>
+    ../../2configs/hw/tp-x2x0.nix
   ];
   # the laptop only has the touchpad
   services.xserver.synaptics.additionalOptions = lib.mkForce ''Option "TouchpadOff" "0"'';
+  hardware.enableRedistributableFirmware = true;
 }
