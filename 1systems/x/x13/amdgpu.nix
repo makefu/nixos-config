0c5b20465e92f474d2268d54e214ace4f5a19ea7
makefu <github@syntax-fehler.de>
2023-06-21 22:41:02 +0200

    ma x13: pull out amdgpu

---
+++ 1systems/x/x13/amdgpu.nix
@@ -0,0 +1,13 @@
+{
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  hardware.opengl.driSupport = true;
+  hardware.opengl.extraPackages = [ pkgs.amdvlk pkgs.rocm-opencl-icd pkgs.rocm-opencl-runtime ];
+  # For 32 bit applications
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+  # is required for amd graphics support ( xorg wont boot otherwise )
+  users.groups.video = {};
+  users.groups.render = {};
+  users.users.makefu.extraGroups = [ "video" "render" ];
+}
