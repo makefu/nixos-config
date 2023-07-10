cd38301aa948d0deaa93ca9b7de902da40796320
makefu <github@syntax-fehler.de>
2023-07-10 18:34:09 +0200

    disko: add simple single-disk partitioning

---
+++ 2configs/fs/disko/single-disk-ext4.nix
@@ -0,0 +1,38 @@
+{ disk ? "/dev/sda", ... }: {
+  disko.devices = {
+    disk = {
+      disk1 = {
+        device = disk;
+        type = "disk";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "500MiB";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            }
+            {
+              name = "root";
+              start = "500MiB";
+              end = "100%";
+              part-type = "primary";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
+}
