0e8741a7d4786ce9ea136a7c84c4df373a1f4594
makefu <github@syntax-fehler.de>
2023-09-03 14:24:26 +0200

    secrets/ssh_server: deploy rsa and ed25519 keys

--- 2configs/secrets/ssh_server.nix
+++ 2configs/secrets/ssh_server.nix
@@ -1,8 +1,8 @@
-{
+{ config, ... }: {
 
   sops.secrets."ssh_host_rsa_key" = {};
   sops.secrets."ssh_host_ed25519_key" = {};
-  services.openssh.hostKeys = lib.mkForce [
-    { bits = 4096; path = (config.sops.secrets."ssh_host_rsa_key".path); type = "rsa"; }
+  services.openssh.hostKeys = [
+    { bits = 4096; path = config.sops.secrets."ssh_host_rsa_key".path; type = "rsa"; }
     { path = config.sops.secrets."ssh_host_ed25519_key".path; type = "ed25519"; } ];
 }
