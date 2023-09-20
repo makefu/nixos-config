b663b76f735c1c062da9a501d3add86aa0242a42
makefu <github@syntax-fehler.de>
2023-09-20 17:41:09 +0200

    savarcast: do not deploy stork search database as root but as stork user

--- 2configs/bgt/savarcast/comments.nix
+++ 2configs/bgt/savarcast/comments.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }:
 let
   configFile = config.sops.secrets."isso.conf".path;
+  searchdir = "/var/www/search";
 in {
 
   sops.secrets."isso.conf" = {
@@ -12,8 +13,21 @@ in {
     group = "isso";
     isSystemUser = true;
   };
+
+  users.users.stork = {
+    group = "stork";
+    isNormalUser = true;
+    home = searchdir;
+    createHome = false;
+    openssh.authorizedKeys.keys = [
+      # GitHub deploy search (bgt_github_deploy.pub)
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGrj6cLVxv6LR0INj2OL/EVdEFMZSk0fOc0pCeXVTirz hi@l33t.name"
+    ];
+  };
+
   users.groups.isso = {};
 
+  systemd.tmpfiles.rules = [ "d ${searchdir} 0770 stork nginx - -" ];
 
   services.isso.enable = true;
   # override the startup to allow secrets in the configFile
@@ -26,6 +40,12 @@ in {
   systemd.services.isso.serviceConfig.ExecStart = lib.mkForce "${pkgs.isso}/bin/isso -c ${configFile}" ;
   systemd.services.isso.serviceConfig.DynamicUser = lib.mkForce false;
 
+  services.nginx.virtualHosts."search.binaergewitter.de" = {
+    locations."/" = {
+      root = "/var/www/search/";
+      tryFiles = "/bgt.st =404"; 
+    };
+  };
   # savarcast is behind traefik, do not configure tls
   services.nginx.virtualHosts."comments.binaergewitter.de" = {
     locations."= /bgt.st".root = "/var/www/search/";
