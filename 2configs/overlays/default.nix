f3ca2fdcdddbb616caa546d18d2779c254b931d2
makefu <github@syntax-fehler.de>
2023-10-02 00:42:44 +0200

    secrets: do not load them from secrets/ anymore, but use clan

--- 2configs/overlays/default.nix
+++ 2configs/overlays/default.nix
@@ -1,14 +1,14 @@
-{ self, ... }:
+{ self,pkgs, ... }:
 {
   nixpkgs.overlays = [
     self.overlays.default
-    self.inputs.stockholm.overlays.default
-    self.inputs.nix-writers.overlays.default
+    (import (self.inputs.stockholm.inputs.nix-writers + "/pkgs"))
     (self: super: {
       inherit (self.writers) writeDash writeDashBin;
-      stockholm.lib = stockholm.lib;
-      ha-ara-menu = self.inputs.ha-ara-menu.packages.${system}.default;
-      inventory4ce = self.inputs.inventory4ce.packages.${system}.default;
+      stockholm.lib = self.inputs.stockholm.lib;
+      ha-ara-menu = self.inputs.ha-ara-menu.packages.${pkgs.system}.default;
+      inventory4ce = self.inputs.inventory4ce.packages.${pkgs.system}.default;
     })
+    self.inputs.stockholm.overlays.default
   ];
 }
