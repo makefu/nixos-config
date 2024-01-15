5e3b53744eeaf1a836eb504d5647ab3180419088
makefu <github@syntax-fehler.de>
2024-01-15 22:35:23 +0100

    flake.nix: use latest clan

--- flake.nix
+++ flake.nix
@@ -1,23 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     clan-core = {
       url = "git+https://git.clan.lol/clan/clan-core";
       # Don't do this if your machines are on nixpkgs stable.
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    clan-core-unstable = {
-      url = "git+https://git.clan.lol/clan/clan-core";
-      # Don't do this if your machines are on nixpkgs stable.
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
+    #home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -54,42 +48,62 @@
   };
   description = "Flake of makefu";
 
-  outputs = { self, nixpkgs, lanzaboote, disko, nixos-hardware, nix-ld, clan-core,nixpkgs-unstable,
-              stockholm, home-manager, nix-writers, vscode-server, ...}@inputs: 
+  outputs = { self, nixpkgs, lanzaboote, disko, nixos-hardware, nix-ld, clan-core,
+               home-manager, nix-writers, vscode-server, ...}@inputs: 
   let
-    inherit (nixpkgs) lib;
+    inherit (nixpkgs) lib pkgs;
+    pkgsForSystem = system: (import nixpkgs {
+      inherit system;
+      #system = "x86_64-linux";
+      config.allowUnfree = true;
+      config.packageOverrides = lib.mkForce (pkgs: { tinc = pkgs.tinc_pre; });
+      config.allowUnfreePredicate = pkg: lib.packageName pkg == "unrar";
+      overlays = [
+        self.overlays.default
+        inputs.nix-writers.overlays.default
+        (import (inputs.stockholm.inputs.nix-writers + "/pkgs"))
+        (this: super: {
+          inherit (this.writers) writeDash writeDashBin;
+          stockholm.lib = inputs.stockholm.lib;
+          ha-ara-menu = inputs.ha-ara-menu.packages.${system}.default;
+          inventory4ce = inputs.inventory4ce.packages.${system}.default;
+        })
+        inputs.stockholm.overlays.default
+      ];
+    });
+    #pkgsForSystem = system: nixpkgs.legacyPackages.${system};
     clan = clan-core.lib.buildClan {
+      clanName = "makefu";
       directory = self;
       specialArgs = {
         inherit (inputs) nixos-hardware self stockholm nixpkgs;
+        inherit inputs;
       };
       machines = lib.genAttrs [ "filepimp" "mrdavid" "x" "cake" "tsp" "wbob" "omo" "gum" "savarcast" ] (host: rec {
         # TODO inject the system somewhere else
-        nixpkgs.hostPlatform = if host == "cake" then  "aarch64-linux" else "x86_64-linux";
+        #nixpkgs.hostPlatform = if host == "cake" then  "aarch64-linux" else "x86_64-linux";
+        nixpkgs.pkgs = if host == "cake" then pkgsForSystem "aarch64-linux" else pkgsForSystem "x86_64-linux";
         imports = [
           disko.nixosModules.disko
           nix-ld.nixosModules.nix-ld
           home-manager.nixosModules.default
           lanzaboote.nixosModules.lanzaboote
 
-          stockholm.nixosModules.brockman
-
-          stockholm.nixosModules.exim-retiolum
-          stockholm.nixosModules.exim
-
-          stockholm.nixosModules.krebs
-          stockholm.nixosModules.hosts
-          stockholm.nixosModules.users
-          stockholm.nixosModules.build
-          stockholm.nixosModules.dns
-          stockholm.nixosModules.kartei
-          stockholm.nixosModules.sitemap
-          stockholm.nixosModules.fetchWallpaper
-          stockholm.nixosModules.git
-          stockholm.nixosModules.tinc
-          stockholm.nixosModules.systemd
-          stockholm.nixosModules.setuid
-          stockholm.nixosModules.urlwatch
+          inputs.stockholm.nixosModules.brockman
+          inputs.stockholm.nixosModules.exim-retiolum
+          inputs.stockholm.nixosModules.exim
+          inputs.stockholm.nixosModules.krebs
+          inputs.stockholm.nixosModules.hosts
+          inputs.stockholm.nixosModules.users
+          inputs.stockholm.nixosModules.build
+          inputs.stockholm.nixosModules.dns
+          inputs.stockholm.nixosModules.kartei
+          inputs.stockholm.nixosModules.sitemap
+          inputs.stockholm.nixosModules.git
+          inputs.stockholm.nixosModules.tinc
+          inputs.stockholm.nixosModules.systemd
+          inputs.stockholm.nixosModules.setuid
+          inputs.stockholm.nixosModules.urlwatch
 
           self.nixosModules.default
           vscode-server.nixosModules.default
@@ -114,7 +128,7 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in pkgs.mkShell {
           packages = [
-            inputs.clan-core-unstable.packages.x86_64-linux.clan-cli
+            inputs.clan-core.packages.x86_64-linux.clan-cli
             pkgs.age
           ];
     };
