26ea6067733293a9355eaa25913b8b7c35b905bf
makefu <github@syntax-fehler.de>
2024-01-15 22:40:02 +0100

    bluetooth-mpd: use pipewire

--- 2configs/bluetooth-mpd.nix
+++ 2configs/bluetooth-mpd.nix
@@ -8,16 +8,23 @@ in {
     default = "/data/music";
     type = lib.types.str;
   };
+
   config = {
+    # pipewire workaround for mpd to play music on kiosk user
+    services.mpd.user = "kiosk";
+    systemd.services.mpd.environment = {
+      # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/609
+      XDG_RUNTIME_DIR = "/run/user/${toString config.users.users.kiosk.uid}";
+    };
+
     services.mpd = {
       enable = true;
       inherit (cfg) musicDirectory;
       network.listenAddress = "0.0.0.0";
       extraConfig = ''
         audio_output {
-          type    "pulse"
-          name    "Local MPD"
-          server  "127.0.0.1"
+          type "pipewire"
+          name "pipewire output"
         }
       '';
     };
@@ -31,38 +38,6 @@ in {
     };
 
     sound.enable = true;
-    hardware.pulseaudio = {
-      enable = true;
-      package = pkgs.pulseaudioFull;
-      # systemWide = true;
-      support32Bit = true;
-      zeroconf.discovery.enable = true;
-      zeroconf.publish.enable = true;
-      tcp = {
-        enable = true;
-        # PULSE_SERVER=192.168.1.11 pavucontrol
-        anonymousClients.allowAll = true;
-        anonymousClients.allowedIpRanges =  [ "127.0.0.1" "192.168.0.0/16" ];
-      };
-      configFile = pkgs.writeText "default.pa" ''
-        load-module module-udev-detect
-        load-module module-bluetooth-policy
-        load-module module-bluetooth-discover
-        load-module module-native-protocol-unix auth-anonymous=1
-        load-module module-always-sink
-        load-module module-console-kit
-        load-module module-systemd-login
-        load-module module-intended-roles
-        load-module module-position-event-sounds
-        load-module module-filter-heuristics
-        load-module module-filter-apply
-        load-module module-switch-on-connect
-        load-module module-equalizer-sink
-        load-module module-dbus-protocol
-        #load-module module-bluez5-device
-        #load-module module-bluez5-discover
-      '';
-    };
     # connect via https://nixos.wiki/wiki/Bluetooth#Using_Bluetooth_headsets_with_PulseAudio
     hardware.bluetooth.enable = true;
     # environment.etc."bluetooth/audio.conf".text = ''
