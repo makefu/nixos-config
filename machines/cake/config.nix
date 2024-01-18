3e72e19cb14f04912c30fbef9f44f13eeb3fd0c4
makefu <github@syntax-fehler.de>
2024-01-18 01:17:01 +0100

    x: add facts

--- machines/cake/config.nix
+++ machines/cake/config.nix
@@ -6,7 +6,7 @@ in {
     ./hardware-config.nix
     ../../2configs
     ../../2configs/home-manager
-    ../../2configs/home/3dprint/
+    ../../2configs/home/3dprint
     #./hardware-config.nix
     { environment.systemPackages = with pkgs;[ rsync screen curl git tmux picocom mosh ];}
     # ../../2configs/tools/core.nix
