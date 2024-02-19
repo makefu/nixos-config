8576c64f5f4c3dce870c626664183c5b3d3196a7
makefu <github@syntax-fehler.de>
2024-02-19 21:07:28 +0100

    gui/wbob: disable sleeping

--- 2configs/gui/wbob-kiosk.nix
+++ 2configs/gui/wbob-kiosk.nix
@@ -40,4 +40,9 @@
       server.address = [ "unix:native" "tcp:4713" ]
     }
   '';
+  # disable sleep
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
