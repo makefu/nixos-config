3c8aeba7fd334f2da2e9c7863b1d83293383588c
makefu <github@syntax-fehler.de>
2024-05-11 02:06:15 +0200

    nix-community: split mediawiki-matrix-bot into legacy and normal

--- 2configs/nix-community/legacy-mediawiki-matrix-bot.nix
+++ 2configs/nix-community/legacy-mediawiki-matrix-bot.nix
@@ -1,24 +1,24 @@
 { pkgs, config, ... }:
 
 {
-  sops.secrets."mediawikibot-config-nixos.org.json" = {
+  sops.secrets."mediawikibot-config-nixos.wiki.json" = {
     mode = "0440";
     group = config.users.groups.mediawiki.name;
   };
   users.groups.mediawiki = {};
 
-  systemd.services.mediawiki-matrix-bot-nixos-org = {
-    description = "Mediawiki Matrix Bot (nixos.org)";
+  systemd.services.mediawiki-matrix-bot-nixos-wiki = {
+    description = "Mediawiki Matrix Bot (nixos.wiki)";
     after = [ "network-online.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       Restart = "always";
       RestartSec = "60s";
       DynamicUser = true;
-      StateDirectory = "mediawiki-matrix-bot-nixos.org";
+      StateDirectory = "mediawiki-matrix-bot-nixos.wiki";
       SupplementaryGroups = [ config.users.groups.mediawiki.name ];
 
-      ExecStart = "${pkgs.mediawiki-matrix-bot}/bin/mediawiki-matrix-bot ${config.sops.secrets."mediawikibot-config-nixos.org.json".path}";
+      ExecStart = "${pkgs.mediawiki-matrix-bot}/bin/mediawiki-matrix-bot ${config.sops.secrets."mediawikibot-config-nixos.wiki.json".path}";
       PrivateTmp = true;
     };
   };
