9f6f4205daaacae484309a60b6f00af1434825c9
makefu <github@syntax-fehler.de>
2024-05-31 10:38:37 +0200

    fs: remove boot.loader.grub

--- 2configs/fs/single-partition-ext4.nix
+++ 2configs/fs/single-partition-ext4.nix
@@ -3,7 +3,6 @@
   # fdisk /dev/sda
   # mkfs.ext4 -L nixos /dev/sda1
   boot.loader.grub.enable = assert config.boot.loader.grub.device != ""; true;
-  boot.loader.grub.version = 2;
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
