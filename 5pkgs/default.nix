66d2369111a2f629a6b11aa36e14011168bc7da6
makefu <github@syntax-fehler.de>
2024-05-31 20:40:34 +0200

    pkgs: overlay with final,prev

--- 5pkgs/default.nix
+++ 5pkgs/default.nix
@@ -1,10 +1,10 @@
-self: super:
-with super.lib; with builtins; let
+final: prev:
+with prev.lib; with builtins; let
   # This callPackage will try to detect obsolete overrides.
   callPackage = path: args: let
-    override =  super.callPackage path args;
+    override =  prev.callPackage path args;
     upstream = optionalAttrs (override ? "name")
-      (super.${(parseDrvName override.name).name} or {});
+      (prev.${(parseDrvName override.name).name} or {});
   in if upstream ? "name" &&
         override ? "name" &&
         compareVersions upstream.name override.name != -1
@@ -20,12 +20,12 @@ with super.lib; with builtins; let
               (filterAttrs (_: eq "directory") (readDir path));
 
 in {
-    quodlibet = verrideDerivation super.quodlibet (old: {
+    quodlibet = verrideDerivation prev.quodlibet (old: {
       doCheck = false; # 1 error because of warnings (possibly upstream)
       patches = [ ./custom/quodlibet/single-digit-discnumber.patch
                   ./custom/quodlibet/remove-override-warning.patch ];
     });
-    #rclone = super.pkgs.lib.overrideDerivation super.rclone (old: {
+    #rclone = prev.pkgs.lib.overrideDerivation prev.rclone (old: {
     #  postInstall = old.postInstall + ''
 
     #        $out/bin/rclone genautocomplete zsh _rclone
@@ -36,20 +36,20 @@ in {
     #});
 
     # Hotfix for https://github.com/NixOS/nixpkgs/issues/157543
-    collectd = super.collectd.override { xen = null; };
+    collectd = prev.collectd.override { xen = null; };
 
     alsa-hdspconf = callPackage ./custom/alsa-tools { alsaToolTarget="hdspconf";};
     alsa-hdspmixer = callPackage ./custom/alsa-tools { alsaToolTarget="hdspmixer";};
     alsa-hdsploader = callPackage ./custom/alsa-tools { alsaToolTarget="hdsploader";};
     brother_ql_web = (builtins.getFlake "github:makefu/brother_ql_web?rev=a3f8625f48111da8cd6f8e562c966cdca445b82d").packages.x86_64-linux.default;
-    qcma = super.libsForQt5.callPackage ./custom/qcma { };
+    qcma = prev.libsForQt5.callPackage ./custom/qcma { };
     inherit (callPackage ./devpi {}) devpi-web ;
-    nodemcu-uploader = super.pkgs.callPackage ./nodemcu-uploader {};
-    liveproxy = super.pkgs.python3Packages.callPackage ./custom/liveproxy {};
-    mediawiki-matrix-bot = super.pkgs.python3Packages.callPackage ./custom/mediawiki-matrix-bot {};
-    hydra-check = super.pkgs.python3Packages.callPackage ./custom/hydra-check {};
-    pwqgen-ger = super.pkgs.passwdqc-utils.override {
-      wordset-file = super.pkgs.fetchurl {
+    nodemcu-uploader = prev.pkgs.callPackage ./nodemcu-uploader {};
+    liveproxy = prev.pkgs.python3Packages.callPackage ./custom/liveproxy {};
+    mediawiki-matrix-bot = prev.pkgs.python3Packages.callPackage ./custom/mediawiki-matrix-bot {};
+    hydra-check = prev.pkgs.python3Packages.callPackage ./custom/hydra-check {};
+    pwqgen-ger = prev.pkgs.passwdqc-utils.override {
+      wordset-file = prev.pkgs.fetchurl {
         urls = [
           https://gist.githubusercontent.com/makefu/b56f5554c9ef03fe6e09878962e6fd8d/raw/1f147efec51325bc9f80c823bad8381d5b7252f6/wordset_4k.c
           https://archive.org/download/nixos-stockholm-tarballs/pviar5j1gxiqcf3l34b4n2pil06xc8zf-wordset_4k.c
