cb06fee357e019763122bd5f1e44ebf1f49bd296
makefu <github@syntax-fehler.de>
2024-05-31 10:49:40 +0200

    signal-rest: bind to host

--- 2configs/hw/slaesh.nix
+++ 2configs/hw/slaesh.nix
@@ -4,6 +4,6 @@
 
   users.users.${config.krebs.build.user.name}.extraGroups = [ "dialout" ];
   services.udev.extraRules = ''
-    SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="zigbee", MODE="0660", GROUP="dailout"
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="zigbee", MODE="0660", GROUP="dialout"
   '';
 }
