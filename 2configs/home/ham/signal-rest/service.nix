3e7e503ff1fcfb1bfbd3708226975b44242920b1
makefu <github@syntax-fehler.de>
2024-05-31 10:49:40 +0200

    signal-rest: bind to host

--- 2configs/home/ham/signal-rest/service.nix
+++ 2configs/home/ham/signal-rest/service.nix
@@ -10,11 +10,13 @@ in {
   state = [ config ];
   virtualisation.oci-containers.containers.signal-rest = {
     image = image;
-    ports = [ "127.0.0.1:${toString port}:8080" ];
+    #ports = [ "127.0.0.1:${toString port}:${toString port}" ];
+    extraOptions = [ "--network=host"];
     volumes = [
       "${config}:/home/.local/share/signal-cli"
     ];
+    environment.PORT = toString port;
     environment.MODE ="json-rpc";
-    #environment.MODE ="native"; # only required for reigstration
+    #environment.MODE ="native"; # only required for registration
   };
 }
