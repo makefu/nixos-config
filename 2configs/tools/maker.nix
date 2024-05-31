bac8d6a842cb6f27b80c3e51207c6eaceed8f592
makefu <github@syntax-fehler.de>
2024-05-31 10:53:35 +0200

    tools: use old cura again

--- 2configs/tools/maker.nix
+++ 2configs/tools/maker.nix
@@ -12,28 +12,7 @@
     # slicing
     #cura
     chitubox
-        (let cura5 = appimageTools.wrapType2 rec {
-      name = "cura5";
-      version = "5.4.0";
-      src = fetchurl {
-        url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-modern.AppImage";
-        hash = "sha256-QVv7Wkfo082PH6n6rpsB79st2xK2+Np9ivBg/PYZd74=";
-      };
-      extraPkgs = pkgs: with pkgs; [ ];
-    }; in writeScriptBin "cura" ''
-      #! ${pkgs.bash}/bin/bash
-      # AppImage version of Cura loses current working directory and treats all paths relateive to $HOME.
-      # So we convert each of the files passed as argument to an absolute path.
-      # This fixes use cases like `cd /path/to/my/files; cura mymodel.stl anothermodel.stl`.
-      args=()
-      for a in "$@"; do
-        if [ -e "$a" ]; then
-          a="$(realpath "$a")"
-        fi
-        args+=("$a")
-      done
-      exec "${cura5}/bin/cura5" "''${args[@]}"
-    '')
+    cura
   ];
   xdg.portal.enable = true;
   #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
