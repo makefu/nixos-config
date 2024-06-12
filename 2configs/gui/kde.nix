b0b29f1af792d3c4dd55402fbab17fd74958b8b8
makefu <github@syntax-fehler.de>
2024-06-12 22:42:55 +0200

    kde: init

---
+++ 2configs/gui/kde.nix
@@ -0,0 +1,7 @@
+{
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  programs.dconf.enable = true;
+}
