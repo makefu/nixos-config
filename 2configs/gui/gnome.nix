5499ebeaf5157ede174ecdf3d73053f9f9a58c70
makefu <github@syntax-fehler.de>
2024-06-20 22:53:07 +0200

    hyprland: move to subdir

--- 2configs/gui/gnome.nix
+++ 2configs/gui/gnome.nix
@@ -11,6 +11,7 @@ in
   services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
   programs.gnome-terminal.enable = true;
   services.xserver = {
+    enable = true;
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
     #displayManager.autoLogin = {
---
+++ 2configs/gui/hyprland/kitty.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, ... }: 
+let
+  mainUser = config.krebs.build.user.name;
+in {
+  # Terminal
+  home-manager.users.${mainUser} = {
+    programs.kitty = {
+      enable = true;
+      font = {
+        package = pkgs.terminus_font;
+        name = "Terminus";
+        size = 12;
+      };
+      settings = {
+        update_check_interval = 0;
+        enable_audio_bell = false;
+        scollback_lines = 100000;
+        confirm_os_window_close = 0;
+        copy_on_select = "yes";
+      };
+    };
+  };
+}
