088b37e974709a3dd9ccae2e79618548728867b8
makefu <github@syntax-fehler.de>
2025-04-21 17:10:56 +0200

    ollama: add home directory, ensure it exists

--- 2configs/llm/ollama.nix
+++ 2configs/llm/ollama.nix
@@ -1,17 +1,21 @@
 let
   uiport = 11435;
   port = 11434;
+  home = "/data/ollama"; # on wbob
 in {
   services.ollama = {
     enable = true;
     acceleration = false; # CPU
     host = "0.0.0.0";
-    inherit port;
-    home = "/data/ollama"; # on wbob
+    inherit port home;
   };
   services.nextjs-ollama-llm-ui = {
     enable = true;
+    hostname = "0.0.0.0";
     port = uiport;
   };
+  systemd.tmpfiles.rules = [
+    "d ${home} 0777 root root - -"
+  ];
   networking.firewall.allowedTCPPorts = [ uiport port ];
 }
