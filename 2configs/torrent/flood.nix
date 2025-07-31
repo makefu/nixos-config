49207c2b8272a41095f47d91f412c9c942e6c1a5
makefu <github@syntax-fehler.de>
2025-07-31 21:57:28 +0200

    flood: init

---
+++ 2configs/torrent/flood.nix
@@ -0,0 +1,32 @@
+let
+  web-port = 8112;
+in {
+  sops.secrets."torrent-auth" = {
+    owner = "nginx";
+    sopsFile = ../../secrets/torrent.yaml;
+  };
+  services.flood = {
+    enable = true;
+    port = web-port;
+    extraArgs = ["--auth=none" "--rtsocket=${config.services.rtorrent.rpcSocket}"];
+  };
+  systemd.services.flood.serviceConfig = {
+    SupplementaryGroups = [ "download" ];
+    DynamicUser = lib.mkForce false;
+    User = config.services.rtorrent.user;
+  };
+  services.nginx = {
+    enable = true;
+    virtualHosts."torrent.${config.krebs.build.host.name}.r" = {
+      basicAuthFile = config.sops.secrets."torrent-auth".path;
+      #root = "${pkgs.nodePackages.flood}/lib/node_modules/flood/dist/assets";
+      #locations."/api".extraConfig = ''
+      #  proxy_pass       http://localhost:${toString web-port};
+      #'';
+      #locations."/".extraConfig = ''
+      #  try_files $uri /index.html;
+      #'';
+      locations."/".proxyPass = "http://localhost:${toString config.flood.port}";
+    };
+  };
+}
--- 2configs/torrent/rtorrent.nix
+++ 2configs/torrent/rtorrent.nix
@@ -2,46 +2,28 @@
 
 let
   peer-port = 51412;
-  web-port = 8112;
   daemon-port = 58846;
   dldir = config.makefu.dl-dir;
 in {
+  imports = [
+    ./flood.nix
+  ];
   services.rtorrent = {
     enable = true;
-    user = "rtorrent";
+    #user = "download";
     port = peer-port;
     # package = pkgs.jesec-rtorrent;
     openFirewall = true;
     group = "download";
+    dataPermissions = "0775";
     downloadDir = dldir;
     configText = ''
       schedule2 = watch_start, 10, 10, ((load.start, (cat, (cfg.watch), "/media/cloud/watch/*.torrent")))
+      pieces.memory.max.set = 1800M
+      network.xmlrpc.size_limit.set = 16M
     '';
   };
-  services.flood = {
-    enable = true;
-    port = web-port;
-    extraArgs = ["--auth=none" "--rtsocket=${config.services.rtorrent.rpcSocket}"];
-  };
   # allow access to the socket
-  systemd.services.flood.serviceConfig.SupplementaryGroups = [ "download" ];
 
   #security.acme.certs."torrent.${config.krebs.build.host.name}.r".server = config.krebs.ssl.acmeURL;
-  sops.secrets."torrent-auth" = {
-    owner = "nginx";
-    sopsFile = ../../secrets/torrent.yaml;
-  };
-  services.nginx = {
-    enable = true;
-    virtualHosts."torrent.${config.krebs.build.host.name}.r" = {
-      basicAuthFile = config.sops.secrets."torrent-auth".path;
-      root = "${pkgs.nodePackages.flood}/lib/node_modules/flood/dist/assets";
-      locations."/api".extraConfig = ''
-        proxy_pass       http://localhost:${toString web-port};
-      '';
-      locations."/".extraConfig = ''
-        try_files $uri /index.html;
-      '';
-    };
-  };
 }
