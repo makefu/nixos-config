3c7168aa1f3dc0d3c9374bd5fc5acc90de57a187
makefu <github@syntax-fehler.de>
2025-10-30 13:21:11 +0100

    matrix frontend: init

--- 2configs/bam/matrix/server.nix
+++ 2configs/bam/matrix/server.nix
@@ -1,14 +1,23 @@
-{config, ... }:
+{config, lib, ... }:
 let
     db-dir = "/media/silent/db/continuwuity";
     user = config.services.matrix-continuwuity.user;
+    statedir = config.services.matrix-continuwuity.settings.global.database_path;
+
 in
 {
     # create matrix db-dir
-    systemd.tmpfiles.settings."matrix-db-dir".d = {
+    systemd.tmpfiles.settings."01-matrix-db-dir"."${db-dir}".d = {
         inherit user;
         mode = "0700";
         group = "root";
+    };
+    # symlink to statedir
+    systemd.tmpfiles.settings."02-matrix-db-symlink".${statedir}."L+" = {
+        inherit user;
+        mode = "0700";
+        group = "root";
+        argument = db-dir;
     };
     sops.secrets.matrix_registration_token.owner = user;
     services.matrix-continuwuity = {
@@ -19,7 +28,13 @@ in
             allow_registration = true;
             registration_token  = config.sops.secrets.matrix_registration_token.path;
             allow_federation = false;
-            database_path = db-dir;
         };
+    };
+    # use oure created statedir symlink instead of the statedirectory mechanism
+    systemd.services.continuwuity.serviceConfig = {
+        StateDirectory = lib.mkForce false;
+        DynamicUser = lib.mkForce false;
+        PrivateMounts = lib.mkForce false;
+        ProtectSystem = lib.mkForce "full";
     };
 }
