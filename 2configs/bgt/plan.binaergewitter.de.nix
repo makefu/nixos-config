2a66cb17753544e1a314f6582812a1e0b8c4ac9b
makefu <github@syntax-fehler.de>
2026-01-19 17:21:04 +0100

    plan.binaergewitter.de: first working version

--- 2configs/bgt/plan.binaergewitter.de.nix
+++ 2configs/bgt/plan.binaergewitter.de.nix
@@ -1,33 +1,43 @@
 { config, lib, pkgs, ... }:
 # more than just nginx config but not enough to become a module
 let
-  port = 8672;
+	port = 8000;
+	domain = "plan.binaergewitter.de";
 in {
   sops.secrets.bgt-datefinder-env = {};
-  services.redis.enable = true;
+  #services.redis.enable = true;
   systemd.services.datefinder = {
     description = "Datefinder Server (bgt)";
     after = [ "network-online.target" ];
     wantedBy = [ "multi-user.target" ];
     environment = {
-        REDIS_URL = "redis://localhost:6379/0";
+        #REDIS_URL = "redis://localhost:6379/0";
+		SITE_URL=   "https://${domain}";
+		ALLOWED_HOSTS=domain;
+		USE_X_FORWARDED_HOST="true";
+		TRUST_PROXY_HEADERS="true";
     };
-    environmentFile = config.sops.secrets.datefinder-env.path;
+    script = ''
+        set -x
+        . "$CREDENTIALS_DIRECTORY/config"
+        export KEYCLOAK_SERVER_URL KEYCLOAK_REALM KEYCLOAK_CLIENT_ID KEYCLOAK_CLIENT_SECRET SECRET_KEY
+        "${pkgs.datefinder}/bin/datefinder-server" migrate
+        "${pkgs.datefinder}/bin/datefinder-server"
+    '';
     serviceConfig = {
+      LoadCredential = [
+          "config:${config.sops.secrets.bgt-datefinder-env.path}"
+      ];
       Restart = "always";
       RestartSec = "60s";
       DynamicUser = true;
       StateDirectory = "datefinder";
       WorkingDirectory = "/var/lib/datefinder";
-
-      ExecStart = "${pkgs.date-finder}/bin/datefinder-server";
       PrivateTmp = true;
     };
   };
-  services.nginx.virtualHosts."plan.binaergewitter.de" = {
+  services.nginx.virtualHosts."${domain}" = {
     # useACMEHost = "euer.krebsco.de";
-    enableACME = true;
-    forceSSL = true;
     locations."/" = {
         proxyPass = "http://127.0.0.1:${toString port}";
         #basicAuthFile = config.sops.secrets.etherpad_htpasswd.path;
