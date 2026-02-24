7f2a744090d96dc5e0d45ec55b93d6d1fdf0504c
makefu <github@syntax-fehler.de>
2026-02-24 09:28:31 +0100

    hyprland: try to fix ssh-agent

--- 2configs/gui/hyprland/passwords.nix
+++ 2configs/gui/hyprland/passwords.nix
@@ -3,13 +3,22 @@ let
     mainUser = config.krebs.build.user.name;
     share = "/home/makefu/.local/share";
 in {
-  # Terminal
   home-manager.users.${mainUser} = {
-    services.ssh-agent.enable = true;
+    # disable standalone ssh-agent; gnome-keyring provides SSH agent functionality
+    services.ssh-agent.enable = false;
     programs.rbw = {
       enable = true;
       settings.base_url = "bw.euer.krebsco.de";
       settings.email = "makefu@x";
+    };
+    # start gnome-keyring-daemon with SSH component and propagate SSH_AUTH_SOCK
+    wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh"
+      ];
+      env = [
+        "SSH_AUTH_SOCK,$XDG_RUNTIME_DIR/keyring/ssh"
+      ];
     };
   };
 
@@ -24,7 +33,8 @@ in {
   services.gnome.gnome-keyring.enable = true;
   # must be set to the greeter in use
   security.pam.services.sddm.enableGnomeKeyring = true;
-  # according to https://old.reddit.com/r/NixOS/comments/1bbjqcn/how_to_enable_gnomekeyring_in_hyprland/
+  # unlock gnome-keyring when resuming from hyprlock
+  security.pam.services.hyprlock.enableGnomeKeyring = true;
   environment.systemPackages = [pkgs.libsecret];
   environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID";
 }
