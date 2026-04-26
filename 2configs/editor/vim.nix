34519d63313b572fb33f4a22d09cf76a521bcd6a
makefu <github@syntax-fehler.de>
2026-04-26 23:58:29 +0200

    editor/vim: use packages instead of strings

--- 2configs/editor/vim.nix
+++ 2configs/editor/vim.nix
@@ -7,13 +7,13 @@
       name = "vim";
       vimrcConfig.customRC = builtins.readFile ./vimrc;
       vimrcConfig.packages.myVimPackage = with pkgs.vimPlugins; { start = [
-       "undotree"
-        "YouCompleteMe"
+       undotree
+        YouCompleteMe
         #"UltiSnips"
         # vim-nix handles indentation better but does not perform sanity
-          "vim-nix"
-          "vim-addon-nix"
-          "vim-better-whitespace"
+          vim-nix
+          vim-addon-nix
+          vim-better-whitespace
       ];
       };
     })
