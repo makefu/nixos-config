d5a96cf8373f7c50cdb7cbbe3e8814cb35530a66
makefu <github@syntax-fehler.de>
2026-05-19 10:01:56 +0200

    rss: install the karakeep-button freshrss extension
    Add the veverkap/xExtension-karakeep-button extension so feed entries
    can be one-click forwarded to karakeep from the freshrss UI.

--- 2configs/deployment/rss/rss.euer.krebsco.de.nix
+++ 2configs/deployment/rss/rss.euer.krebsco.de.nix
@@ -10,6 +10,20 @@ in {
         passwordFile = config.sops.secrets.rss-password.path;
         virtualHost = fqdn;
         baseUrl = "https://${fqdn}";
+        extensions = [
+          (pkgs.freshrss-extensions.buildFreshRssExtension {
+            FreshRssExtUniqueId = "karakeep-button";
+            pname = "karakeep-button";
+            version = "unstable-2025-11-26";
+            src = pkgs.fetchFromGitHub {
+              owner = "veverkap";
+              repo = "xExtension-karakeep-button";
+              rev = "8ccdace7266aa94a4795a97d311a8b494d779785";
+             hash = "sha256-h0XnCSoH2djxNyo363CMz7+ZeHAhaA4G3naea2M5Pw8=";
+           };
+          })
+
+        ];
     };
   #services.tt-rss = {
   #  enable = true;
