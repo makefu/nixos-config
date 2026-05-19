2fd14d92dd0d3f92b79b6b93c32590db44ccb9f2
makefu <github@syntax-fehler.de>
2026-05-19 10:02:13 +0200

    systemdultras/ircbot: disable rss-bridge and brockman services
    Both services are currently unused and pulling them in just keeps an
    idle web vhost and IRC bot online; comment the definitions out rather
    than deleting them so the wiring is still around when we want to bring
    them back.

--- 2configs/systemdultras/ircbot.nix
+++ 2configs/systemdultras/ircbot.nix
@@ -1,38 +1,38 @@
 { pkgs,config,inputs, ... }: {
   #systemd.services.brockman.environment."BROCKMAN_LOG_LEVEL" = "DEBUG";
 
-  services.rss-bridge = {
-    enable = true;
-    whitelist = [ "*" ];
-    virtualHost = "rss.makefu.r";
-  };
+  #services.rss-bridge = {
+  #  enable = true;
+  #  whitelist = [ "*" ];
+  #  virtualHost = "rss.makefu.r";
+  #};
 
-  services.brockman = {
-    enable = true;
-    package = inputs.brockman.packages.${pkgs.stdenv.hostPlatform.system}.default;
-    config = {
-      channel = "#systemdultras";
-      irc = {
-        host = "irc.hackint.org";
-        port = 6697;
-        tls = true;
-      };
-      notifyErrors = false;
-      bots = {
-        r-systemdultras-rss = {
-          feed = "https://www.reddit.com/r/systemdultras/.rss";
-          delay = 236;
-        };
-        r-systemd-rss = {
-          feed = "https://www.reddit.com/r/systemd/.rss";
-          delay = 272;
-        };
-        r-pid_eins-mastodon = {
-          feed = "https://mastodon.social/users/pid_eins.rss";
-          delay = 621;
-        };
-      };
-    };
+  #services.brockman = {
+  #  enable = true;
+  #  package = inputs.brockman.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  #  config = {
+  #    channel = "#systemdultras";
+  #    irc = {
+  #      host = "irc.hackint.org";
+  #      port = 6697;
+  #      tls = true;
+  #    };
+  #    notifyErrors = false;
+  #    bots = {
+  #      r-systemdultras-rss = {
+  #        feed = "https://www.reddit.com/r/systemdultras/.rss";
+  #        delay = 236;
+  #      };
+  #      r-systemd-rss = {
+  #        feed = "https://www.reddit.com/r/systemd/.rss";
+  #        delay = 272;
+  #      };
+  #      r-pid_eins-mastodon = {
+  #        feed = "https://mastodon.social/users/pid_eins.rss";
+  #        delay = 621;
+  #      };
+  #    };
+  #  };
 
-  };
+  #};
 }
