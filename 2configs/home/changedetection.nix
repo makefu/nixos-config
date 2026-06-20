bd1f68bb895dcbcfb6dbe03bf2b8e772f584cfa8
makefu <github@syntax-fehler.de>
2026-06-20 17:50:57 +0200

    changedetection: serve via nginx on change.euer

--- 2configs/home/changedetection.nix
+++ 2configs/home/changedetection.nix
@@ -1,5 +1,5 @@
 let
-    port = 5000;
+    port = 5000; # 
 in {
     services.changedetection-io = {
         enable = true;
@@ -7,6 +7,18 @@ in {
         #playwrightSupport = true;
         listenAddress = "0.0.0.0";
         inherit port;
-        baseURL = "http://change.lan:${ toString port }";
+        baseURL = "http://change.euer";
     };
+
+  services.nginx.virtualHosts."change" = {
+    serverAliases = [
+      "change.euer"
+      "change.lan"
+    ];
+
+    locations."/" = {
+      proxyPass = "http://localhost:${toString port}";
+      proxyWebsockets = true;
+    };
+  };
 }
